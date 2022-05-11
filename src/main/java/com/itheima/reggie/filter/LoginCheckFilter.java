68c6cbbdd89778621b5c9688282eb9c060720b50
18135808049 <1045412025@qq.com>
2022-05-11 17:08:09 +0800

    改善登录功能:用户未登录访问其他系统中的界面会直接跳转到登陆页面

---
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,68 @@
+package com.itheima.reggie.filter;
+
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author cxw
+ * @date 2022/5/11 15:49
+ */
+@Slf4j
+@WebFilter
+public class LoginCheckFilter implements Filter {
+
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestURI = request.getRequestURI();
+
+        log.info("拦截到请求:{}",requestURI);
+
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        boolean check = check(urls, requestURI);
+
+        if(check){
+            log.info("请求{}不需要拦截",requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(request.getSession().getAttribute("employee")!=null){
+            log.info("用户已登录,用户id为{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+        }
+
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOT LOGIN")));
+
+    }
+
+    public boolean check(String[] urls, String requestURI){
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match){
+                return true;
+            }
+        }
+        return false;
+    }
+}
