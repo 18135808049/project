e6443b50a865fdd26353c9ee4c64cde88bb6ec19
18135808049 <1045412025@qq.com>
2022-05-11 08:50:44 +0800

    update gitignore

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -14,5 +14,6 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功");
+        log.info("aaaaa");
     }
 }
---
+++ src/main/java/com/itheima/reggie/entity/employee.java
@@ -0,0 +1,28 @@
+package com.itheima.reggie.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author cxw
+ * @date 2022/5/11 10:15
+ */
+@Data
+public class employee {
+    private Long id;
+    private String name;
+    private String password;
+    private String phone;
+    private String sex;
+    private String idNumber;
+    private Integer status;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
