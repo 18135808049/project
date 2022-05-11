a8f0465e8849b340d0a029f713c87e5c5b04a588
18135808049 <1045412025@qq.com>
2022-05-11 10:20:24 +0800

    update gitignore

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -14,6 +14,5 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功");
-        log.info("aaaaa");
     }
 }
